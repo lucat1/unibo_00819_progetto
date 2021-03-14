78f587cc63e626b760b47d9b0a969e9a20c9cbe6
Luca <lucat1@users.noreply.github.com>
2021-03-14 13:45:29 +0100

    showcase the new vert_box

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "engine/ui/pad_box.hpp"
 #include "engine/ui/screen.hpp"
 #include "engine/ui/text_box.hpp"
+#include "engine/ui/vert_box.hpp"
 #include <curses.h>
 #include <iostream>
 using namespace std;
@@ -21,29 +22,12 @@ int main() {
        {Engine::UI::PADDING_TOP, 2},
        {Engine::UI::PADDING_BOTTOM, 2}});
   List *list = append<List, const wchar_t>(pbox, 1, 1, L'*');
-  append<TextBox, const wchar_t *>(
-      list, 1, 1,
-      L"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
-      L"eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad "
-      L"minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-      L"aliquip ex ea commodo consequat. Duis aute irure dolor in "
-      L"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-      L"pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-      L"culpa qui officia deserunt mollit anim id est laborum.");
-  PadBox *pbox1 = append<PadBox, map<enum PAD, uint16_t>>(
-      list, 1, 1,
-      {{Engine::UI::PADDING_LEFT, 4},
-       {Engine::UI::PADDING_RIGHT, 4},
-       {Engine::UI::PADDING_TOP, 2},
-       {Engine::UI::PADDING_BOTTOM, 2}});
-  append<TextBox, const wchar_t *>(
-      pbox1, 1, 1,
-      L"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
-      L"eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad "
-      L"minim veniam, quis nostrud exercitation ullamco laboris nisi ut ");
-  append<TextBox, const wchar_t *>(list, 1, 1, L"Premere q per uscire");
-  append<TextBox, const wchar_t *>(list, 1, 1,
-                                   L"PS: il latino fa altamente cagare");
+  for (int i = 0; i < 21; i++) {
+    VertBox *vbox = append<VertBox>(list, 1, 1);
+    wstring str = L"Text on the left " + to_wstring(i);
+    append<TextBox, const wchar_t *>(vbox, .7, 1, str.c_str());
+    append<TextBox, const wchar_t *>(vbox, .3, 1, L"Text on the right");
+  }
 
   if (screen->open()) {
     cout << "Error while opening screen" << endl;
