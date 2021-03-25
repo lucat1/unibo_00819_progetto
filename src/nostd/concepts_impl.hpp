d4cb966a431cec349c0f5256e7913601da4783bd
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-03-25 16:55:54 +0100

    Concepts cleanup; matrix tests setup

--- src/nostd/concepts_impl.hpp
+++ src/nostd/concepts_impl.hpp
@@ -6,14 +6,12 @@
   Stefano Volpe #969766
   03/19/2021
 
-  concepts_impl.hpp: implementer interface of generic programming concepts
-  (which will be introduced as an actual language facility only in C++20)
+  concepts_impl.hpp: implementer helpers for generic programming concepts (which
+  will be introduced as an actual language facility only in C++20)
 */
 
 #ifndef NOSTD_CONCEPTS_IMPL_HPP
 #define NOSTD_CONCEPTS_IMPL_HPP
-
-#include "concepts.hpp"
 
 #include <type_traits>
 
@@ -44,8 +42,6 @@ public:
 template <class T>
 struct Has_equal_trait : SubstitutionSucceeded<class Equal_result<T>::type> {};
 
-template <class T> constexpr bool Has_equal();
-
 template <class T> struct Not_equal_result {
 private:
   template <class X>
@@ -59,8 +55,6 @@ public:
 template <class T>
 struct Has_not_equal_trait
     : SubstitutionSucceeded<class Equal_result<T>::type> {};
-
-template <class T> constexpr bool Has_not_equal();
 
 template <class T> struct Right_shift_result {
 private:
@@ -76,8 +70,6 @@ template <class T>
 struct Has_right_shift_trait
     : SubstitutionSucceeded<class Right_shift_result<T>::type> {};
 
-template <class T> constexpr bool Has_right_shift();
-
 template <class T> struct Left_shift_result {
 private:
   template <class X>
@@ -91,34 +83,6 @@ public:
 template <class T>
 struct Has_left_shift_trait
     : SubstitutionSucceeded<class Left_shift_result<T>::type> {};
-
-template <class T> constexpr bool Has_left_shift();
-
-template <class T> constexpr bool Boolean();
-
-template <class T> constexpr bool Equality_comparable();
-
-template <class T> constexpr bool Movable();
-
-template <class T> constexpr bool Copyable();
-
-/*
-  A type is said to be "semiregular" when it is both default constructible and
-  copyable.
-*/
-template <class T> constexpr bool Semiregular();
-
-/*
-  A type is said to be "regular" when it is both semiregular and equality
-  comparable.
-*/
-template <class T> constexpr bool Regular();
-
-/*
-  A type is said to be "streamable" when it is both input and output
-  streamable.
-*/
-template <class T> constexpr bool Streamable();
 
 } // namespace Nostd
 
---
+++ src/nostd/matrix.test.cpp
@@ -0,0 +1,23 @@
+/*
+  University of Bologna
+  First cicle degree in Computer Science
+  00819 - Programmazione
+
+  Stefano Volpe #969766
+  03/25/2021
+
+  matrix.test.cpp: tests for Nostd::Matrix and its helper entities.
+*/
+
+#include "matrix.hpp"
+#include <cassert>
+#include <iostream>
+
+using namespace Nostd;
+using std::cout;
+
+int main() {
+  // TODO
+}
+
+#include "matrix.cpp"
