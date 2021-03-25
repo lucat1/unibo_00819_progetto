d8f38b6f4f4cfd8feb12eefcb12f9a502b8f3920
Luca <lucat1@users.noreply.github.com>
2021-03-25 19:53:49 +0100

    more tests, found a bug

--- src/nostd/vector.test.cpp
+++ src/nostd/vector.test.cpp
@@ -1,6 +1,7 @@
-#include <assert.h>
 #include "../util/test.hpp"
+#include "pair.hpp"
 #include "vector.hpp"
+#include <assert.h>
 using namespace Nostd;
 
 int main() {
@@ -9,11 +10,13 @@ int main() {
     assert(v.size() == 0);
     assert(v.capacity() == 1);
   });
+
   it("constructs a vector with a given size", {
     Vector<int> v(58);
     assert(v.size() == 58);
     assert(v.capacity() == 87);
   });
+
   it("constructs a vector containing n times the given element", {
     Vector<int> v(20, 79);
     assert(v.size() == 20);
@@ -21,11 +24,13 @@ int main() {
     for (size_t i = 0; i < v.size(); i++)
       assert(v[i] == 79);
   });
-  it("adds one element to the array", {
+
+  it("adds one element to the vector", {
     Vector<int> v;
     v.push_back(1337);
     assert(v.at(0) == 1337);
   });
+
   it("cleans the vector", {
     Vector<int> v;
     v.push_back(124);
@@ -35,6 +40,7 @@ int main() {
     assert(v.size() == 0);
     assert(v.capacity() == 1);
   });
+
   it("removes one element from the vector", {
     Vector<int> v;
     v.push_back(124);
@@ -44,5 +50,23 @@ int main() {
     assert(v.size() == 2);
     assert(v[0] == 124);
     assert(v[1] == 1594);
+  });
+
+  it("resizes the vector to the given length", {
+    Vector<int> v;
+    for (int i = 0; i < 100; i++)
+      v.push_back(i);
+
+    v.resize(50);
+    assert(v.size() == 50);
+    for (int i = 0; i < 50; i++)
+      assert(v[i] == i);
+  });
+
+  it("accepts complex elements as values", {
+    Vector<Pair<int, int>> v;
+    Pair<int, int> p = {1, 1};
+    v.push_back(p);
+    assert(v[0] == p);
   });
 }
