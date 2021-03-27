6272143803131a3f474a0a7642f348eb39808f45
= <mattia.porco981@gmail.com>
2021-03-28 00:08:03 +0100

    hotfix: test

--- src/nostd/map.test.cpp
+++ src/nostd/map.test.cpp
@@ -9,76 +9,76 @@ using namespace std;
 using namespace Nostd;
 
 int main() {
-  it("umap: add", {
+  it("umap: put", {
     Nostd::UnorderedMap<int, int> um;
-    um.add(2, 5);
-    um.add(4, 10);
-    um.add(1, 23);
-    um.add(5, 111);
+    um.put(2, 5);
+    um.put(4, 10);
+    um.put(1, 23);
+    um.put(5, 111);
   });
   it("umap: operator []", {
     Nostd::UnorderedMap<int, int> um;
-    um.add(2, 5);
+    um.put(2, 5);
     um[2] = 10;
     assert(um[2] == 10);
   });
 
   it("umap: get_values", {
     Nostd::UnorderedMap<int, int> um;
-    um.add(2, 5);
-    um.add(4, 25);
+    um.put(2, 5);
+    um.put(4, 25);
     auto v = um.get_values();
     assert(v.size() == 2);
   });
 
   it("umap contains", {
     Nostd::UnorderedMap<int, int> um;
-    um.add(4, 5);
+    um.put(4, 5);
     assert(um.contains(4));
   });
 
   it("umap: remove", {
     Nostd::UnorderedMap<int, int> um;
-    um.add(4, 5);
-    um.add(10, 15);
+    um.put(4, 5);
+    um.put(10, 15);
     um.remove(4);
     assert(um.get_values().size() == 1);
   });
 
   // Treemap
 
-  it("tmap: add", {
+  it("tmap: put", {
     Nostd::TreeMap<int, int> um;
-    um.add(2, 5);
-    um.add(4, 10);
-    um.add(1, 23);
-    um.add(5, 111);
+    um.put(2, 5);
+    um.put(4, 10);
+    um.put(1, 23);
+    um.put(5, 111);
   });
   it("tmap: operator []", {
     Nostd::TreeMap<int, int> um;
-    um.add(2, 5);
+    um.put(2, 5);
     um[2] = 10;
     assert(um[2] == 10);
   });
 
   it("tmap: get_values", {
     Nostd::TreeMap<int, int> um;
-    um.add(2, 5);
-    um.add(4, 25);
+    um.put(2, 5);
+    um.put(4, 25);
     auto v = um.get_values();
     assert(v.size() == 2);
   });
 
   it("tmap: contains", {
     Nostd::TreeMap<int, int> um;
-    um.add(4, 5);
+    um.put(4, 5);
     assert(um.contains(4));
   });
 
   it("tmap: remove", {
     Nostd::TreeMap<int, int> um;
-    um.add(4, 5);
-    um.add(14, 10);
+    um.put(4, 5);
+    um.put(14, 10);
     um.remove(4);
     cout << um.get_values().size() << endl;
     assert(um.get_values().size() == 1);
