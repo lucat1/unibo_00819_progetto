fd037af23c99f64a307cdfd9f164bf9964764175
Luca <lucat1@users.noreply.github.com>
2021-03-28 11:53:42 +0200

    first button sytling

--- src/engine/ui/center.cpp
+++ src/engine/ui/center.cpp
@@ -16,14 +16,28 @@ void Engine::UI::Center::propb(Box::Property key, bool value) {
 
 void Engine::UI::Center::show(WINDOW *window, uint16_t x, uint16_t y) {
   if (center_horizontally)
-    Box::show(window, x + std::floor((max_width - Box::size().first) / 2), y);
+    Box::show(window, x + std::floor((max_child_width - Box::size().first) / 2),
+              y);
   else
-    Box::show(window, x, y + std::floor((max_height - Box::size().second) / 2));
+    Box::show(window, x,
+              y + std::floor((max_child_height - Box::size().second) / 2));
 }
 
 Nostd::Pair<uint16_t, uint16_t> Engine::UI::Center::size() {
+  uint16_t width = 0, height = 0;
   if (center_horizontally)
-    return {max_width, Box::size().second};
+    width = max_child_width;
   else
-    return {Box::size().first, max_height};
+    height = max_child_height;
+
+  for (Box *it = first_child; it != nullptr; it = it->sibling) {
+    auto child_size = it->size();
+    if (center_horizontally)
+      height = std::max(height, child_size.second);
+    else
+      width = std::max(width, child_size.first);
+  }
+
+  uint16_t w = width + pl + pr, h = height + pt + pb;
+  return {w, h};
 }
