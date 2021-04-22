5c658fb67d16c1d3ddb14896e76f52bc9d12a86c
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-04-22 22:57:23 +0200

    Data::Map to Data::MapChunk

--- src/data/map_chunk.test.cpp
+++ src/data/map_chunk.test.cpp
@@ -6,24 +6,24 @@
   Stefano Volpe #969766
   04/09/2021
 
-  map.test.cpp: tests for Data::Map.
+  map_chunk.test.cpp: tests for Data::MapChunk.
 */
 
 #include <cassert>
 
 #include "../nostd/test.hpp"
-#include "map.hpp"
+#include "map_chunk.hpp"
 
-using Data::Map;
+using Data::MapChunk;
 using Data::MapUnit;
 using Nostd::it;
 
 int main() {
   it("constructs a Map", [] {
-    Map m(2, MapUnit::ground);
+    MapChunk m(2, MapUnit::ground);
     for (auto &row : m)
       for (auto &cell : row)
         assert(cell.value() == MapUnit::ground);
   });
-  it("returns the right width", [] { assert(Map(33).width() == 33); });
+  it("returns the right width", [] { assert(MapChunk(33).width() == 33); });
 }
