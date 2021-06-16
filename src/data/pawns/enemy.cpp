186c94700144f659d8028280abc03815628d7236
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-06-17 01:21:37 +0200

    Enemy user interface implementation

---
+++ src/data/pawns/enemy.cpp
@@ -0,0 +1,10 @@
+/*
+  University of Bologna
+  First cicle degree in Computer Science
+  00819 - Programmazione
+
+  Stefano Volpe #969766
+  06/16/2021
+
+  enemy.cpp: implementation of Data::Pawns::Enemy and its helper entities.
+*/
