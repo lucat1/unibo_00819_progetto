8fa196cc36c9bd945a9082f227f816e8ec972e2d
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-06-17 13:23:47 +0200

    projectile.hpp boilerplate code

---
+++ src/data/pawns/projectile.hpp
@@ -0,0 +1,32 @@
+/*
+  University of Bologna
+  First cicle degree in Computer Science
+  00819 - Programmazione
+
+  Stefano Volpe #969766
+  06/17/2021
+
+  projectile.hpp: user interface of Data::Pawns::Projectile.
+*/
+
+#ifndef DATA_PAWNS_PROJECTILE_HPP
+#define DATA_PAWNS_PROJECTILE_HPP
+
+#include <istream>
+
+#include "../../engine/colorable.hpp"
+#include "../../nostd/wstring.hpp"
+#include "interactable.hpp"
+
+namespace Data {
+
+namespace Pawns {
+
+/*
+  A Projectile can be thrown by a Skill in order to inflict indirect damage.
+*/
+}
+
+} // namespace Data
+
+#endif
