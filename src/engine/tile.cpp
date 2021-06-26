429f5dbd4907d6face9968eceabfe6baebc711fe
Mattia <mgiro2001@gmail.com>
2021-06-26 15:45:38 +0000

    tile

--- src/engine/tile.cpp
+++ src/engine/tile.cpp
@@ -3,15 +3,24 @@
   First cicle degree in Computer Science
   00819 - Programmazione
 
-  Luca Tagliavini #971133
-  06/12/21
-
   Stefano Volpe #969766
   06/12/21
+
+  Mattia Girolimetto #977478
+  06/26/21
 
   tile.cpp: implementation of Engine::Tile.
 */
 
 #include "tile.hpp"
 
-wchar_t Engine::Tile::character() const { return L' '; }
+using Engine::Tile;
+
+wchar_t Tile::character() const { return L' '; }
+
+bool Tile::operator==(const Tile &tile) const {
+  return character() == tile.character() && background() == tile.background() &&
+         foreground() == tile.foreground();
+}
+
+bool Tile::operator!=(const Tile &tile) const { return !(*this == tile); }
