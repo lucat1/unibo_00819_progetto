300fb851469364b73d3443054a02c44331f5d6ab
mattia <mattia.porco981@gmail.com>
2021-08-05 23:18:40 +0200

    pr comments

--- src/world/position.cpp
+++ src/world/position.cpp
@@ -10,11 +10,15 @@
 */
 
 #include "position.hpp"
+#include "../data/map_chunk.hpp"
+
 using namespace World;
 using namespace Nostd;
 using namespace Engine;
 
 Position::Position(const List<Matrix<BlockTile *>> *environment, int x, int y) {
+  this->x = x;
+  this->y = y;
   this->environment = environment;
   this->fragment = environment->begin();
 }
@@ -25,14 +29,17 @@ void Position::move_left() {
   else if (*fragment != *environment->begin()) {
     fragment = std::prev(fragment);
     x = (*fragment).extent(1) - 1;
-  } else
-    throw InvalidPositionException(x, y);
+  }
+  // else we are in the first column of the first chunk so we do nothing
 }
 
 void Position::move_right() {
   if ((size_t)x != (*fragment).extent(1) - 1)
     x++;
   else {
+    if (*fragment == *environment->end()) {
+      // TODO generate more chunk
+    }
     fragment = std::next(fragment);
     x = 0;
   }
@@ -43,7 +50,9 @@ List<Matrix<BlockTile *>>::iterator Position::get_fragment() const noexcept {
 }
 void Position::move_up() { y = std::max(y - 1, 0); }
 
-void Position::move_down() { y = std::min(y + 1, 24); }
+void Position::move_down() {
+  y = std::min(y + 1, (int)Data::MapChunk::height - 1);
+}
 
 int Position::get_x() const noexcept { return this->x; }
 int Position::get_y() const noexcept { return this->y; }
