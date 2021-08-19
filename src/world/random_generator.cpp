0f76aa24e067ed498fb8c800bdfd5651e5e73cb8
mattia <mattia.porco981@gmail.com>
2021-08-19 15:57:51 +0200

    working on random

--- src/world/random_generator.cpp
+++ src/world/random_generator.cpp
@@ -10,13 +10,32 @@
 */
 
 #include "random_generator.hpp"
+#include <cstddef>
+#include <random>
+#include <stdexcept>
 
 using namespace World;
 
-RandomGenerator::RandomGenerator() : seed(time(nullptr)) { srand(this->seed); }
+RandomGenerator::RandomGenerator() : seed(time(nullptr)), random_engine(seed) {
+  srand(seed);
+}
 
-const size_t RandomGenerator::get_random(const size_t &bound) const noexcept {
+const size_t RandomGenerator::get_random(size_t bound) const noexcept {
   return rand() % bound;
 }
 
-time_t RandomGenerator::get_seed() const noexcept { return this->seed; }
+time_t RandomGenerator::get_seed() const noexcept { return seed; }
+
+const size_t RandomGenerator::get_poisson_random(size_t mean, size_t bound) {
+  if (!mean || mean > bound)
+    throw std::invalid_argument(
+        "Invalid mean: mean can't be 0 or greater than bound");
+  std::poisson_distribution<size_t> distribution(mean);
+  const int n = bound - distribution(random_engine) - 1;
+  return n < 0 ? bound : n;
+}
+
+const size_t RandomGenerator::get_poisson_random_reverse(size_t mean,
+                                                         size_t bound) {
+  return get_poisson_random(bound - mean, bound);
+}
--- src/world/random_generator.hpp
+++ src/world/random_generator.hpp
@@ -12,21 +12,30 @@
 #ifndef WORLD_RANDOM_GENERATOR_HPP
 #define WORLD_RANDOM_GENERATOR_HPP
 
+#include <cstddef>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 
 namespace World {
 
 class RandomGenerator {
 private:
   const time_t seed;
+  std::default_random_engine random_engine;
 
 public:
   // Init random with seed from current time
   RandomGenerator();
 
   // Get random number between 0 and bound parameter
-  const size_t get_random(const size_t &bound) const noexcept;
+  const size_t get_random(size_t bound) const noexcept;
+
+  // Designed for items
+  const size_t get_poisson_random(size_t mean, size_t bound);
+
+  // Designed for enemies
+  const size_t get_poisson_random_reverse(size_t mean, size_t bound);
 
   // Seed filed getter
   time_t get_seed() const noexcept;
